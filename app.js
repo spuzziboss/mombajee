ae3a27d457728bf369bee78817c0e1832f6049ec
spuzziboss <spuzziboss@gmail.com>
2017-09-28 18:53:53 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -111,11 +111,9 @@ app.set('view engine', 'pug');
 
 
 
+var port = process.env.port || 3000;
 
-app.listen(3000,function() {
- console.log('Listening on port 3000');
- 
-});
+app.listen(port);
 
  
 
