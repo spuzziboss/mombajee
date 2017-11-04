3d61e8253a5de84d0abddfaa5221c800b7215436
spuzziboss <spuzziboss@gmail.com>
2017-11-04 11:59:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -100,11 +100,11 @@ app.use(mdb);
 
 
 
-var favicon = require('serve-favicon');
-app.use(favicon(path.join(__dirname, 'public/images/', 'favicon.ico')));
+
 
 app.use(express.static(path.join(__dirname, 'public')));
-
+var favicon = require('serve-favicon');
+app.use(favicon(path.join(__dirname, 'public/images/', 'favicon.ico')));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
